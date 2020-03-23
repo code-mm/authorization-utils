962dda86cdf58b72c1b18e8f2aeb617758628ceb
m-maohuawei <maohuawei@sina.com>
2020-03-23 21:08:33 +0800

    添加注释

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        // 替换成自己的账号
         String name = "xxxxxx@xxxx.com";
 
+        // 替换成自己的密码
         String password = "xxxxxx";
 
         String authString = name + ":" + password;
